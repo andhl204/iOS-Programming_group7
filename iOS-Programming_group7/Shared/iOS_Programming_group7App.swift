1b9e09dc2212d6090d54d78f60014130e2fd54fe
andhl <andhlwhffu@gmail.com>
2021-11-29 00:45:47 +0900

    feat url to image

---
+++ iOS-Programming_group7/Shared/iOS_Programming_group7App.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Programming_group7App.swift
+//  Shared
+//
+//  Created by SWY on 2021/11/19.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Programming_group7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
